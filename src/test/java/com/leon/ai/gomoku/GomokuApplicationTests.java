76c1fb825ca2e25669ae74248c98e8c37a562058
niufei01 <niufei01@baidu.com>
2023-07-07 19:05:20 +0800

    init

---
+++ src/test/java/com/leon/ai/gomoku/GomokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.ai.gomoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
